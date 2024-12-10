1195a6014d28637ba4d96dd3afbe19bec6a0648c
daniel-budai <danielbudai@tutanota.com>
2024-12-10 16:43:28 +0100

    created first test file, created handlers for confirmation and create booking

--- src/__mocks__/handlers.js
+++ src/__mocks__/handlers.js
@@ -1,3 +1,35 @@
-// import { http, HttpResponse } from "msw";
+import { http, HttpResponse } from "msw";
 
-export const handlers = [];
+export const handlers = [
+  http.post(
+    "https://h5jbtjv6if.execute-api.eu-north-1.amazonaws.com",
+    async ({ request }) => {
+      const booking = await request.json();
+
+      const price =
+        parseInt(booking.people) * 120 + parseInt(booking.lanes) * 100;
+
+      const confirmation = {
+        id: "012345",
+        when: booking.when,
+        time: booking.time,
+        lanes: booking.lanes,
+        people: booking.people,
+        shoes: booking.shoes,
+        price: price,
+        active: true,
+      };
+
+      sessionStorage.setItem("confirmation", JSON.stringify(confirmation));
+      return HttpResponse.json(confirmation);
+    }
+  ),
+
+  http.get(
+    "https://h5jbtjv6if.execute-api.eu-north-1.amazonaws.com/confirmation",
+    () => {
+      const storedConfirmation = sessionStorage.getItem("confirmation");
+      return HttpResponse.json(JSON.parse(storedConfirmation));
+    }
+  ),
+];
--- src/setupTest.js
+++ src/setupTest.js
@@ -1,10 +1,7 @@
 import { server } from "./__mocks__/server";
 import "@testing-library/jest-dom";
-import { expect, afterEach } from "vitest";
+import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
-import matchers from "@testing-library/jest-dom/matchers";
-
-expect.extend(matchers);
 
 beforeAll(() => {
   server.listen();
